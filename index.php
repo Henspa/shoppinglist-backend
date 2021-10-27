f28d9260c0b6af6a79db90f10dd1af1bd05979a3
henni saaranen <n0sahe00@students.oamk.fi>
2021-10-27 12:40:52 +0300

    backend jako osiin

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <?php
-header('Access-Control-Allow-Origin:*');
-header('Content-Type: application/json');
+require_once 'inc/headers.php';
+require_once 'inc/functions.php';
+require_once 'inc/delete.php';
 
-$db = new PDO('mysql:host=localhost;dbname=shoppinglist;charset=utf8','root','');
-$sql = "select * from item";
-$query = $db->query($sql);
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-header('HTTP/1.1 200 OK');
-print json_encode($results);
+try {
+    $db = openDB();
+
+    $sql = "select * from item";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    header('HTTP/1.1 200 OK');
+    print json_encode($results);
+} catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
